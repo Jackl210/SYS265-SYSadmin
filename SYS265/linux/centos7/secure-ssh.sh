bdffb711708ed54406b30a8c9a7b1fe9309ede05
root <root@web01-jack.jack.local>
2023-02-19 17:48:00 -0500

    SSH Script test

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author Jack Lyons
+#creates a new SSH user using $1 paramter
+#adds a public key from the local repo or curled from the remote repo
+#removes the ability to SSH in
+echo "ALL YOUR CODE GOES HERE"
