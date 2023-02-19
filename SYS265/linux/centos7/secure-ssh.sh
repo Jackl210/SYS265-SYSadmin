8f6d04327cadd3547791e828937b61ad449729ff
Jackl210 <jack.lyons@mymail.Champlain.edu>
2023-02-19 23:37:28 +0000

    ssh-test-02

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -1,6 +1,8 @@
-#secure-ssh.sh
-#author Jack Lyons
-#creates a new SSH user using $1 paramter
-#adds a public key from the local repo or curled from the remote repo
-#removes the ability to SSH in
-echo "ALL YOUR CODE GOES HERE"
+#!/bin/bash
+
+useradd -m -d /home/sys265 -s /bin/bash jack-ssh
+mkdir /home/jack-ssh/.ssh
+cp ~/SYS265-SYSadmin/SYS265/linux/public-keys/id_rsa.pub ~/jack-ssh/.ssh/authorized_keys
+chmod 700 ~/jack-ssh/.ssh
+chmod 600 ~/jack-ssh/.ssh/authorized_keys
+chown -R sys265:sys265 ~/jack-ssh/.ssh
